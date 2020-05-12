4e6dc9d9dc6f6b5659559da4127daa69151e5432
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2020-05-12 23:20:32 +0300

    in

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/dthread/dthread.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/dthread/dthread.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/getaddrinfo_server.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/getaddrinfo_server.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/main.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/main.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/address.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/src/address.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/io_api.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/src/io_api.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/poll.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/src/poll.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/socket.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/src/socket.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/timer.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/server.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/dthread/dthread.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/dthread/dthread.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/getaddrinfo_server.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/getaddrinfo_server.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/gtest/gtest-all.cc" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/gtest/gtest-all.cc.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/gtest/gtest_main.cc" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/gtest/gtest_main.cc.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/address.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/src/address.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/io_api.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/src/io_api.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/poll.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/src/poll.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/socket.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/src/socket.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/src/timer.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/src/timer.cpp.o"
+  "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/test_main.cpp" "/Users/dzhiblavi/Documents/prog/cpp/code/io_api_epoll/build/CMakeFiles/test.dir/test_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
